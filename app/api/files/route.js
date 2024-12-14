f121164a408f3705c98f64779ad139d54e397bb2
joelsinnott <joelsinnott@hotmail.com>
2024-12-14 10:52:44 +0100

    changed route structure for app

--- app/api/files/route.js
+++ app/api/files/route.js
@@ -2,27 +2,30 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
     const directoryPath = path.join(process.cwd(), 'public/uploads');
-    fs.readdir(directoryPath, (err, files) => {
-      if (err) {
-        res.status(500).json({ error: 'Error reading the directory' });
-        return;
-      }
+
+    try {
+      const files = await fs.promises.readdir(directoryPath);
 
       // Map through files and create an array of file data
-      const fileData = files.map((file) => {
-        const stats = fs.statSync(path.join(directoryPath, file));
-        return {
-          name: file,
-          path: `/uploads/${file}`,
-          createdAt: stats.mtime,
-        };
-      });
+      const fileData = await Promise.all(
+        files.map(async (file) => {
+          const stats = await fs.promises.stat(path.join(directoryPath, file));
+          return {
+            name: file,
+            path: `/uploads/${file}`,
+            createdAt: stats.mtime,
+          };
+        })
+      );
 
       res.status(200).json(fileData);
-    });
+    } catch (err) {
+      console.error('Error reading the directory:', err);
+      res.status(500).json({ error: 'Error reading the directory' });
+    }
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,19 +1,23 @@
+// app/api/upload/route.js
+
 import formidable from 'formidable';
 import fs from 'fs';
 import path from 'path';
 
+// Disable body parsing to handle file upload
 export const config = {
   api: {
-    bodyParser: false, // Disable default body parser to handle file upload
+    bodyParser: false,
   },
 };
 
+// Handling the request
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const form = new formidable.IncomingForm();
     const uploadDir = path.join(process.cwd(), '/public/uploads');
-    
-    // Check if the upload directory exists, if not create it
+
+    // Ensure the upload directory exists
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
@@ -23,8 +27,8 @@ export default function handler(req, res) {
 
     form.parse(req, (err, fields, files) => {
       if (err) {
-        console.error('Parsing error:', err);
-        return res.status(500).json({ error: 'Error parsing the file' });
+        console.error('Error parsing file:', err);
+        return res.status(500).json({ error: 'Error parsing file' });
       }
 
       const tempPath = files.file?.[0]?.filepath;
